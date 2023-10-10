2c2173285ce5d57cea2d3766e81977c81a507347
Hargun Oberoi <hargun@ip-172-31-29-248.ec2.internal>
2023-10-10 10:42:15 +0000

    modified model and main file to fix errors in data loading

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #! python3
-#%%
+# %%
 import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_sequence
@@ -9,125 +9,137 @@ import torch.nn.functional as F
 from torch.optim import Adam, AdamW
 from torch.nn.utils import clip_grad_norm_
 from torchtext.data.metrics import bleu_score
-from sacremoses import MosesTokenizer,MosesDetokenizer
+from sacremoses import MosesTokenizer, MosesDetokenizer
 import pandas as pd
 from collections import Counter, namedtuple
 from pathlib import Path
 import numpy as np
 from tqdm import tqdm
-from scripts.dataset import TranslationDataset # The logic of TranslationDataset is defined in the file dataset.py
-from scripts.model import Encoder,Decoder
+from scripts.dataset import (
+    TranslationDataset,
+)  # The logic of TranslationDataset is defined in the file dataset.py
+from scripts.model import Encoder, Decoder
+
+
 # %%
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-token_en = MosesTokenizer(lang='en')
-token_fr = MosesTokenizer(lang='fr')
+token_en = MosesTokenizer(lang="en")
+token_fr = MosesTokenizer(lang="fr")
 
-df = pd.read_csv('data/fra.txt',sep='\t',header=None,names=['en','fr','attribution'])
-df.drop('attribution',axis=1,inplace=True)
+df = pd.read_csv(
+    "data/fra.txt", sep="\t", header=None, names=["en", "fr", "attribution"]
+)
+df.drop("attribution", axis=1, inplace=True)
 
-checkpoint_path = Path('data/checkpoint.pt')
+checkpoint_path = Path("data/checkpoint.pt")
 
 V_s = vocab_source = 2001
 V_t = vocab_target = 5001
 E = 128
 H = 256
 B = 256
-dataset = TranslationDataset(df,V_s,V_t,from_file=True)
+dataset = TranslationDataset(df, V_s, V_t, from_file=True)
 
-#%%
+# %%
+
 
 # %%
 class TranslationNN(nn.Module):
-    def __init__(self,V_s,V_t,E,H):
+    def __init__(self, V_s, V_t, E, H):
         super(TranslationNN, self).__init__()
-        self.encoder = Encoder(V_s,E,H)
-        self.decoder = Decoder(V_t,E,H)
+        self.encoder = Encoder(V_s, E, H)
+        self.decoder = Decoder(V_t, E, H)
 
-    def forward(self, x_s,x_t):
+    def forward(self, x_s, x_t):
         all_h_enc = self.encoder(x_s)
-        out = self.decoder(x_t,all_h_enc)
+        out = self.decoder(x_t, all_h_enc, device)
         return out
+
 
 # %%
 # instantiate params
 lr = learning_rate = 3e-4
-model = TranslationNN(V_s,V_t,E,H)
-optim = AdamW(model.parameters(),lr=lr)
-loss_fn = nn.CrossEntropyLoss(reduction='none')
+model = TranslationNN(V_s, V_t, E, H)
+optim = AdamW(model.parameters(), lr=lr)
+loss_fn = nn.CrossEntropyLoss(reduction="none")
 if checkpoint_path.exists():
     checkpoint = torch.load(checkpoint_path)
-    # load checkpoint details 
-    model.load_state_dict(checkpoint['nn_state'])
-    optim.load_state_dict(checkpoint['opt_state'])
-    epoch = checkpoint['epoch']
-    loss=checkpoint['loss']
+    # load checkpoint details
+    model.load_state_dict(checkpoint["nn_state"])
+    optim.load_state_dict(checkpoint["opt_state"])
+    epoch = checkpoint["epoch"]
+    loss = checkpoint["loss"]
 
 # make dataloaders
 
+
 def collate_fn(batch):
     xs, xt = zip(*batch)
-    xs = [torch.Tensor(x) for x in xs]
-    xt = [torch.Tensor(x[:-1]) for x in xt]
+    x_s = [torch.Tensor(x) for x in xs]
+    x_t = [torch.Tensor(x[:-1]) for x in xt]
     y = [torch.Tensor(x[1:]) for x in xt]
-    xs = pad_sequence(xs, batch_first=True)
-    xt = pad_sequence(xt, batch_first=True)
+    x_s = pad_sequence(x_s, batch_first=True)
+    x_t = pad_sequence(x_t, batch_first=True)
     y = pad_sequence(y, batch_first=True)
-    return xs.long(), xt.long(), y.long()
+    return x_s.long(), x_t.long(), y.long()
 
-train_set, valid_set = random_split(dataset,[0.9,0.1])
-train_loader = DataLoader(train_set,batch_size=B,collate_fn=collate_fn,shuffle=True)
-valid_loader = DataLoader(valid_set,batch_size=B,collate_fn=collate_fn)
+
+train_set, valid_set = random_split(dataset, [0.9, 0.1])
+train_loader = DataLoader(train_set, batch_size=B, collate_fn=collate_fn, shuffle=True)
+valid_loader = DataLoader(valid_set, batch_size=B, collate_fn=collate_fn)
 
 # %%
-if 'epoch' not in locals():
+if "epoch" not in locals():
     epoch = 0
 num_epochs = 1
 
 
-for epoch in range(epoch,epoch+num_epochs):
-    train_loss, eval_loss = torch.zeros(len(train_loader)), torch.zeros(len(valid_loader))
+for epoch in range(epoch, epoch + num_epochs):
+    train_loss, eval_loss = torch.zeros(len(train_loader)), torch.zeros(
+        len(valid_loader)
+    )
     print(f"Epoch {epoch+1}")
-    for c, (x_s,x_t, y) in enumerate(tqdm(train_loader)):
-        x_s,x_t,y = x_s.to(device),x_t.to(device),y.to(device)
+    for c, (x_s, x_t, y) in enumerate(tqdm(train_loader)):
+        x_s, x_t, y = x_s.to(device), x_t.to(device), y.to(device)
         model.to(device)
-        out = model(x_s,x_t)
-        out = out.permute(0,2,1)
-        mask = (y!=0)
+        out = model(x_s, x_t)
+        out = out.permute(0, 2, 1)
+        mask = y != 0
         # check later if the mask is needed
-        loss = loss_fn(out,y)
+        loss = loss_fn(out, y)
         loss = loss[mask].mean()
         # backprop step
         optim.zero_grad()
         loss.backward()
         # clip the gradients
-        clip_grad_norm_(model.parameters(),max_norm=1)
+        clip_grad_norm_(model.parameters(), max_norm=1)
         # optimization step
         optim.step()
         train_loss[c] = loss
     # get the averaged training loss
     train_loss = train_loss.mean()
-    print(f'Training Loss for Epoch {epoch+1} is {train_loss:.4f}')
+    print(f"Training Loss for Epoch {epoch+1} is {train_loss:.4f}")
 
-    for c, (x_s,x_t, y) in enumerate(valid_loader):
+    for c, (x_s, x_t, y) in enumerate(valid_loader):
         with torch.no_grad():
-            device = torch.device('cpu')
-            x_s, x_t,y = x_s.to(device), x_t.to(device), y.to(device)
+            device = torch.device("cpu")
+            x_s, x_t, y = x_s.to(device), x_t.to(device), y.to(device)
             model.to(device)
-            out = model(x_s,x_t)
-            out = out.permute(0,2,1)
-            mask = (y!=0)
-            loss = loss_fn(out,y)
+            out = model(x_s, x_t)
+            out = out.permute(0, 2, 1)
+            mask = y != 0
+            loss = loss_fn(out, y)
             loss = loss[mask].mean()
             eval_loss[c] = loss
 
     # get the averaged validation loss
     eval_loss = eval_loss.mean()
-    print(f'Validation Loss for Epoch {epoch+1} is {eval_loss:.4f}')
-    if checkpoint_path.exists() and eval_loss < checkpoint['loss']:
-        checkpoint['epoch'] = epoch
-        checkpoint['loss'] = eval_loss
-        checkpoint['nn_state'] = model.state_dict()
-        checkpoint['opt_state'] = optim.state_dict()
-        torch.save(checkpoint,checkpoint_path)
+    print(f"Validation Loss for Epoch {epoch+1} is {eval_loss:.4f}")
+    if checkpoint_path.exists() and eval_loss < checkpoint["loss"]:
+        checkpoint["epoch"] = epoch
+        checkpoint["loss"] = eval_loss
+        checkpoint["nn_state"] = model.state_dict()
+        checkpoint["opt_state"] = optim.state_dict()
+        torch.save(checkpoint, checkpoint_path)
 # %%
--- scripts/model.py
+++ scripts/model.py
@@ -9,42 +9,43 @@ import numpy as np
 
 # step 2: define encoder
 class Encoder(nn.Module):
-    def __init__(self,V,E,H):
+    def __init__(self, V, E, H):
         super(Encoder, self).__init__()
-        self.embedding = nn.Embedding(V,E,max_norm=1, scale_grad_by_freq=True)
-        self.gru = nn.GRU(E,H,batch_first=True,bidirectional=True)
+        self.embedding = nn.Embedding(V, E, max_norm=1, scale_grad_by_freq=True)
+        self.gru = nn.GRU(E, H, batch_first=True, bidirectional=True)
 
     def forward(self, x):
-        x = pad_sequence((torch.tensor(i) for i in x),batch_first=True)
-        B,T = x.shape
-        mask_lens = (x!=0).sum(1).to(torch.device('cpu'))
+        B, T = x.shape
+        mask_lens = (x != 0).sum(1).to(torch.device("cpu"))
         emb = self.embedding(x)
-        x_pack = pack_padded_sequence(emb,mask_lens,batch_first=True,enforce_sorted=False)
-        all_h_packed,_ = self.gru(x_pack)
-        all_h, _ = pad_packed_sequence(all_h_packed,batch_first=True)
+        x_pack = pack_padded_sequence(
+            emb, mask_lens, batch_first=True, enforce_sorted=False
+        )
+        all_h_packed, _ = self.gru(x_pack)
+        all_h, _ = pad_packed_sequence(all_h_packed, batch_first=True)
         return all_h
         # return all hidden states
 
-    def evaluate(self,x):
+    def evaluate(self, x):
         with torch.no_grad():
             return self.forward(x)
-        
+
+
 # enc = Encoder(8,4,2)
 # x = [[4,2,2],[3,5]]
 # out = enc(x)
 # %%
 # create AttentionGRU
 
+
 class AddAttention(nn.Module):
-
-    def __init__(self,hidden_size):
+    def __init__(self, hidden_size):
         super().__init__()
-        self.Wa = nn.Linear(hidden_size,hidden_size)
-        self.Ua = nn.Linear(2*hidden_size,hidden_size)
-        self.Va = nn.Linear(hidden_size,1,bias=False)
+        self.Wa = nn.Linear(hidden_size, hidden_size)
+        self.Ua = nn.Linear(2 * hidden_size, hidden_size)
+        self.Va = nn.Linear(hidden_size, 1, bias=False)
 
-
-    def forward(self,query,keys):
+    def forward(self, query, keys):
         """
         keys dim: B,Tx,H
         query dim: B,1,H
@@ -52,47 +53,51 @@ class AddAttention(nn.Module):
         scores = self.Va(torch.tanh(self.Wa(query) + self.Ua(keys))).squeeze(-1)
         scores = scores.unsqueeze(1)
 
-        weights = F.softmax(scores,dim=-1) # B,1,Tx
-        context = weights @ keys # B,1,H
+        weights = F.softmax(scores, dim=-1)  # B,1,Tx
+        context = weights @ keys  # B,1,H
 
-        return context, weights 
+        return context, weights
+
 
 # attn = AddAttention(4)
 # q = torch.randn(4,1,4)
 # k = torch.randn(4,2,8)
 # out = attn(q,k)
-#%%
+# %%
+
 
 class Decoder(nn.Module):
-    def __init__(self,V,E,H):
+    def __init__(self, V, E, H):
         super().__init__()
-        self.embedding = nn.Embedding(V,E)
+        self.embedding = nn.Embedding(V, E)
         self.attention = AddAttention(H)
-        self.gru = nn.GRU(E+2*H,H,batch_first=True)
-        self.out = nn.Linear(H,V)
-        self.initialW = nn.Parameter(torch.randn(H,H))
+        self.gru = nn.GRU(E + 2 * H, H, batch_first=True)
+        self.out = nn.Linear(H, V)
+        self.initialW = nn.Parameter(torch.randn(H, H))
 
-    def forward(self,x,hidden_encoder):
+    def forward(self, x, hidden_encoder, device):
         # made a change to be stashed
-        x = pad_sequence((torch.tensor(i[:-1]) for i in x),batch_first=True)
         H = self.gru.hidden_size
         x_emb = self.embedding(x)
-        B,T,E = x_emb.shape
-        s_prev = hidden_encoder[:,:1,H:] @ self.initialW # shape B,1,H
-        hidden_decoder = torch.zeros(B,T,H)
+        B, T, E = x_emb.shape
+        s_prev = hidden_encoder[:, :1, H:] @ self.initialW  # shape B,1,H
+        hidden_decoder = torch.zeros(B, T, H).to(
+            device
+        )  # hoping that the device defined in main will take care of this
         for t in range(T):
-            x_t = x_emb[:,t,:].unsqueeze(1)
-            c_t,_ = self.attention(s_prev,hidden_encoder)
-            x_in = torch.cat((x_t,c_t),dim=-1)
+            x_t = x_emb[:, t, :].unsqueeze(1)
+            c_t, _ = self.attention(s_prev, hidden_encoder)
+            x_in = torch.cat((x_t, c_t), dim=-1)
             s_prev, _ = self.gru(x_in)
-            hidden_decoder[:,t,:] = s_prev.squeeze(1)
+            hidden_decoder[:, t, :] = s_prev.squeeze(1)
 
         out = self.out(hidden_decoder)
 
         return out
 
+
 # x = [[1,2,3],[2,5]]
 # all_h = torch.randn(2,4,8)
 # dec = Decoder(5,10,4)
 # out = dec(x,all_h)
-#%%
+# %%
