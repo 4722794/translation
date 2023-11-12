5f44ffc6814303ff453778173f926bf3ae766585
Ubuntu <ubuntu@ip-172-31-29-248.ec2.internal>
2023-11-12 20:36:29 +0000

    ready for sweeps over layers

--- main.py
+++ main.py
@@ -43,21 +43,19 @@ checkpoint = init_checkpoint(conf,checkpoint_path,device)
 if not checkpoint_path.exists():
     raise Exception("No checkpoint found.")
 
-# lr finder
-min_lr = get_min_lr(train_path, val_path, test_path, source_tokenizer, target_tokenizer, conf.batch_size, conf.vocab_source, conf.vocab_target, conf.embedding_size, conf.hidden_size, conf.dropout, conf.num_layers, conf.dot_product, conf.optimizer, conf.learning_rate, device)
 
-print(f"New learning rate is {min_lr}")
-
-config["learning_rate"] = min_lr
 #%%
 def main(config=None,project=None,name=None,checkpoint=None):
     
     # keep the entire code within the wandb context manager
 
     with wandb.init(config=config,project=project,name=name):
-        start_time = time.time()
-        # all the code goes here
+        # find learning rate
+        # lr finder
+        min_lr = get_min_lr(train_path, val_path, test_path, source_tokenizer, target_tokenizer, conf.batch_size, conf.vocab_source, conf.vocab_target, conf.embedding_size, conf.hidden_size, conf.dropout, conf.num_layers, conf.dot_product, conf.optimizer, conf.learning_rate, device)
         c = wandb.config
+        c.learning_rate = min_lr
+        # all the code goes here
         # get dataset
         train_set,val_set,test_set = get_dataset(train_path,source_tokenizer,target_tokenizer), get_dataset(val_path,source_tokenizer,target_tokenizer), get_dataset(test_path,source_tokenizer,target_tokenizer)
         # get loaders
@@ -68,10 +66,6 @@ def main(config=None,project=None,name=None,checkpoint=None):
         optim = get_optimizer(model, c.optimizer, c.learning_rate)
         # loss fn
         loss_fn = nn.CrossEntropyLoss(reduction="none")
-        # get the learning rate
-        # set new learning rate
-        c.learning_rate = min_lr
-
         # OPTIONAL: get_scheduler
         scheduler = get_scheduler(optim, c.scheduler)
 
@@ -79,26 +73,20 @@ def main(config=None,project=None,name=None,checkpoint=None):
         num_epochs = c.num_epochs # hard coding this value for now until further discussion
         for epoch in tqdm(range(num_epochs)):
             print(f"Epoch {epoch+1}")
-            train_loss = train_loop(model, train_loader, loss_fn, optim,scheduler,epoch, device,log=False)
+            train_loss = train_loop(model, train_loader, loss_fn, optim,scheduler,epoch, device)
             print(f"Training Loss for Epoch {epoch+1} is {train_loss:.4f}")
             val_loss = valid_loop(model, val_loader, loss_fn, device)
             print(f"Validation Loss for Epoch {epoch+1} is {val_loss:.4f}")
-            # if validation loss lower than checkpoint, save new weights
-            if val_loss < checkpoint["loss"]:
-                checkpoint = save_checkpoint(checkpoint_path, model, epoch,val_loss,optim,scheduler)
+            bleu_score = get_bleu(model, test_loader, device)
+            print(f"Mean BLEU score is {bleu_score:.4f}")
             metrics = {
                 "baseplots/epoch": epoch,
                 "baseplots/train_loss": train_loss,
                 "baseplots/val_loss": val_loss,
+                "baseplots/bleu_score": bleu_score,
             }
-            elapsed_time = time.time() - start_time
             wandb.log(metrics)
-            if elapsed_time > c.max_time:
-                break
+        wandb.log({"bleu":bleu_score})
+
             
-
-# run the experiment
-name = "PSGRU-Paris testrun 2"
-project_name="ucl"
-
 wandb.agent("1c581x2f",main,count=20,project="sweepstakes")
